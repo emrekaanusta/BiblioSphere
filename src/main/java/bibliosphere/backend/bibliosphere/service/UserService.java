40a918cf4326fd4c387151f32d1feae4ece60bf1
Ege Tan <tanege02@gmail.com>
2025-03-04 21:39:09 +0300

    User service class updated - ege.

--- src/main/java/bibliosphere/backend/bibliosphere/service/UserService.java
+++ src/main/java/bibliosphere/backend/bibliosphere/service/UserService.java
@@ -1,4 +1,17 @@
 package bibliosphere.backend.bibliosphere.service;
 
+import bibliosphere.backend.bibliosphere.model.User;
+import bibliosphere.backend.bibliosphere.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User loadUserByEmail (String email) {
+        return userRepository.findById(email).orElse(null);
+    }
 }
