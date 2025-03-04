f2334905fe29bcc7d2daa86d279c647f37eb00ab
Ege Tan <tanege02@gmail.com>
2025-03-04 21:06:24 +0300

    Some user properties added to the User class - ege.

--- src/main/java/bibliosphere/backend/bibliosphere/model/User.java
+++ src/main/java/bibliosphere/backend/bibliosphere/model/User.java
@@ -1,4 +1,15 @@
 package bibliosphere.backend.bibliosphere.model;
 
+import java.util.List;
+
 public class User {
+    private String password;
+    private String email;
+    private String name;
+    private String surname;
+    private String phone;
+    private String avatar;
+    private String bio;
+    private List<String> basket;
+    private List<String> orders;
 }
