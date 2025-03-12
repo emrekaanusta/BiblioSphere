818a79ef739e4a5795991cf9b133c5ca0665da1e
bkiyanfer <berilkiyanfer@gmail.com>
2025-03-12 16:43:53 +0300

    HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
     return (
@@ -9,9 +10,11 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
 
-                <Route path="/login" element={<Login />} />
+                <Route path="/Login" element={<Login />} />
 
-                <Route path="/home" element={<Home />} />
+                <Route path="/Home" element={<Home />} />
+
+                <Route path="/Products" element={<ProductPage />} />
             </Routes>
         </Router>
     );
