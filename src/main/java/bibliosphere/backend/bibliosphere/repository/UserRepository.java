1b323d5160c6f403fbb6e0d9b6967a8cbf77ccbe
Ege Tan <tanege02@gmail.com>
2025-03-18 22:53:57 +0300

    pull from main

--- src/main/java/bibliosphere/backend/bibliosphere/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package bibliosphere.backend.bibliosphere.repository;
-import bibliosphere.backend.bibliosphere.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
